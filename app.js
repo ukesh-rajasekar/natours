550c4b45da81a614fbb36370f7e1417d73df9048
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2022-05-21 12:35:51 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+const app = express();
+
+
+//middlewares 
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('hello from the server');
+    req.requestedTime = new Date().toISOString();
+    next()
+})
+
+const PORT = 3000;
+
+
+
+
+app.listen(PORT, () => {
+
+    console.log(`server is listening at ${PORT}`)
+
+})
+
+const getAllTours = (req, res) => {
+    res.status(200).json({ status: 'success', requestedAt: req.requestedTime, results: tours.length, data: { tours } })
+}
+
+const getTour = (req, res) => {
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        })
+    }
+
+    const tour = tours.find(ele => ele.id === id)
+
+
+    res.status(200).json({ status: 'success', data: { tour: tour } })
+}
+
+const updateTour = (req, res) => {
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        })
+    }
+
+    res.status(200).json({ status: '<updated data here ...>' })
+}
+
+const deleteTour = (req, res) => {
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        })
+    }
+    res.status(204).json({ status: 'success', data: null })
+}
+
+
+const createTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+    })
+}
+
+
+app.get('/api/v1/tours', getAllTours)
+app.get('/api/v1/tours/:id', getTour)
+app.patch('/api/v1/tours/:id', updateTour)
+app.delete('/api/v1/tours/:id', deleteTour)
+app.post('/api/v1/tours', createTour)
