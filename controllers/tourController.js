5dff8c130e8df57bcc2a2ebc38b65aaee0d5f88c
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2022-05-21 16:16:10 +1000

    feat: param middlewares

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,30 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`tour function called with id: ${val}`)
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        })
+    }
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    console.log(`tour create function called`)
+    const { body } = req;
+
+    if (body.name === undefined || body.price === undefined) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'price and name is required'
+        })
+    }
+    next();
+}
 
 //ROUTE HANDLERS
 exports.getAllTours = (req, res) => {
@@ -10,14 +34,6 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
     const id = req.params.id * 1;
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid id'
-        })
-    }
-
     const tour = tours.find(ele => ele.id === id)
 
 
@@ -26,26 +42,13 @@ exports.getTour = (req, res) => {
 
 exports.updateTour = (req, res) => {
     const id = req.params.id * 1;
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid id'
-        })
-    }
-
     res.status(200).json({ status: '<updated data here ...>' })
 }
 
 exports.deleteTour = (req, res) => {
     const id = req.params.id * 1;
 
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid id'
-        })
-    }
+
     res.status(204).json({ status: 'success', data: null })
 }
 
